c5d393c46346163210a1d77068f9fcabc6daaf73
Phil Nguyen <pcn@cs.umd.edu>
2018-03-02 16:02:00 -0500

    fix

--- traces/typed.rkt
+++ traces/typed.rkt
@@ -5,6 +5,6 @@
 ;; TR currently can't generate the required contract.
 ;; Not sure if I want parametric contracts anyways,
 ;; as they would mess with equality in hashtables.
-(unsafe-require/typed/provide "../main.rkt"
+(unsafe-require/typed/provide "main.rkt"
   [function-traces (∀ (α) (α → (Setof α)) α → Void)]
   [hash-traces (∀ (α) (HashTable α (Setof α)) α → Void)])
