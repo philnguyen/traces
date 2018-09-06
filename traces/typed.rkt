54d1388e7126d124549b298d2f390240c887c6e0
Phil Nguyen <pcn@cs.umd.edu>
2018-09-06 12:36:57 -0400

    add missing typed bindings

--- traces/typed.rkt
+++ traces/typed.rkt
@@ -7,4 +7,6 @@
 ;; as they would mess with equality in hashtables.
 (unsafe-require/typed/provide "main.rkt"
   [function-traces (∀ (α) (α → (Setof α)) α → Void)]
-  [hash-traces (∀ (α) (HashTable α (Setof α)) α → Void)])
+  [function-traces/tag (∀ (α) (α → (Setof α)) α → (HashTable Integer α))]
+  [hash-traces (∀ (α) (HashTable α (Setof α)) α → Void)]
+  [hash-traces/tag (∀ (α) (HashTable α (Setof α)) α → (Vectorof α))])
