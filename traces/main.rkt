3b2bb2903f33a51a7ec125a0dbeed0de7d813756
Phil Nguyen <pcn@cs.umd.edu>
2018-09-07 15:02:52 -0400

    display window for lookup tag instead

--- traces/main.rkt
+++ traces/main.rkt
@@ -1,8 +1,12 @@
 #lang racket/base
 
 (require racket/set
+         racket/match
          redex/reduction-semantics
-         redex/gui)
+         redex/gui
+         racket/gui/base
+         (only-in racket/class new send)
+         (only-in racket/port with-input-from-string))
 
 (provide function-traces
          function-traces/tag
@@ -11,19 +15,19 @@
          make-int-tagger)
 
 (define (function-traces f e) (traces (function->reduction f) e))
-(define (hash-traces     h e) (traces (hash->reduction h)     e))
-(define (function-traces/tag f e val->tag tag->val)
-  (define (f* tag)
-    (for/seteq ([val (in-set (f (tag->val tag)))])
-      (val->tag val)))
-  (function-traces f* (val->tag e)))
-(define (hash-traces/tag h e val->tag tag->val)
-  (define h*
-    (for/hasheq ([(k vs) (in-hash h)])
-      (values (val->tag k) (for/seteq ([v (in-set vs)]) (val->tag v)))))
-  (hash-traces h* (val->tag e)))
+(define (hash-traces     h e) (function-traces (hash->function h) e))
+(define (function-traces/tag f e [tagger (call-with-values make-int-tagger cons)])
+  (match-define (cons val->tag tag->val) tagger)
+  (define (f* tag) (for/seteq ([val (in-set (f (tag->val tag)))])
+                     (val->tag val)))
+  (function-traces f* (val->tag e))
+  (thread (λ () (show-lookup-window tag->val))))
+(define (hash-traces/tag h e [tagger (call-with-values make-int-tagger cons)])
+  (function-traces/tag (hash->function h) e tagger))
 
 (define-language MT)
+
+(define ((hash->function h) x) (hash-ref h x seteq))
 
 (define (function->reduction f)
   (let ([idx 0])
@@ -33,17 +37,50 @@
           (where any_name ,(begin (set! idx (+ 1 idx)) idx))
           (where {_ ... any_1 _ ...} ,(set->list (f (term any))))])))
 
-(define (hash->reduction h) (function->reduction (λ (x) (hash-ref h x seteq))))
-
-(define (make-int-tagger #:start [start 0] #:on-new-tag [on-new-tag! (λ (i x) (void))])
+(define (make-int-tagger)
   (define cache:val->int (make-hash))
   (define cache:int->val (make-hasheq))
   (define (val->int val)
     (hash-ref! cache:val->int val
                (λ ()
-                 (define n (+ start (hash-count cache:val->int)))
+                 (define n (hash-count cache:val->int))
                  (hash-set! cache:int->val n val)
-                 (on-new-tag! n val)
                  n)))
   (define (int->val i) (hash-ref cache:int->val i (λ () (error 'int->val "nothing for ~a" i))))
   (values val->int int->val))
+
+(define (show-lookup-window tag->val)
+  (define frame (new frame%
+                     [label "Tag Lookup"]
+                     [width 640]
+                     [height 480]))
+  (define main-panel (new vertical-panel% [parent frame]))
+  (define lookup-panel (new horizontal-panel%
+                            [parent main-panel]
+                            [stretchable-height #f]))
+  (define input (new text-field%
+                     [parent lookup-panel]
+                     [label "Tag"]
+                     [callback
+                      (λ (_ e)
+                        (case (send e get-event-type)
+                          [(text-field-enter) (on-lookup)]))]))
+  (define btn (new button%
+                   [parent lookup-panel]
+                   [label "Lookup"]
+                   [callback
+                    (λ (_ e)
+                      (case (send e get-event-type)
+                        [(button) (on-lookup)]))]))
+  (define content (new text-field%
+                       [parent main-panel]
+                       [label "Value"]
+                       [style '(multiple)]))
+  (define (on-lookup)
+    (define tag (with-input-from-string (send input get-value) read))
+    (match (with-handlers ([exn? (λ _ #f)])
+             (list (tag->val tag)))
+      [(list val)
+       (send content set-value (format "~a" val))]
+      [#f (send content set-value "(nothing)")]))
+  (send frame show #t))
--- traces/typed.rkt
+++ traces/typed.rkt
@@ -7,8 +7,11 @@
 ;; as they would mess with equality in hashtables.
 (unsafe-require/typed/provide "main.rkt"
   [function-traces (∀ (α) (α → (Setof α)) α → Void)]
-  [function-traces/tag (∀ (α) (α → (Setof α)) α (α → Integer) (Integer → α) → Void)]
+  [function-traces/tag
+   (∀ (α) ([(α → (Setof α)) α]
+           [(Pairof (α → Integer) (Integer → α))] . ->* . Void))]
   [hash-traces (∀ (α) (HashTable α (Setof α)) α → Void)]
-  [hash-traces/tag (∀ (α) (HashTable α (Setof α)) α (α → Integer) (Integer → α) → Void)]
-  [make-int-tagger (∀ (α) ([] [#:start Integer #:on-new-tag (Integer α → Any)]
-                              . ->* . (Values (α → Integer) (Integer → α))))])
+  [hash-traces/tag
+   (∀ (α) ([(HashTable α (Setof α)) α]
+           [(Pairof (α → Integer) (Integer → α))] . ->* . Void))]
+  [make-int-tagger (∀ (α) → (Values (α → Integer) (Integer → α)))])
