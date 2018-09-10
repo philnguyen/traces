e6068a14678c8bd9aa99a2aa8e4d0139929fe5a9
Phil Nguyen <pcn@cs.umd.edu>
2018-09-09 21:42:59 -0400

    generalize tag type

--- traces/typed.rkt
+++ traces/typed.rkt
@@ -2,16 +2,13 @@
 
 (require typed/racket/unsafe)
 
-;; TR currently can't generate the required contract.
+(define-type (Bij α β) (Pairof (α → β) (β → α)))
+
 ;; Not sure if I want parametric contracts anyways,
 ;; as they would mess with equality in hashtables.
 (unsafe-require/typed/provide "main.rkt"
   [function-traces (∀ (α) (α → (Setof α)) α → Void)]
-  [function-traces/tag
-   (∀ (α) ([(α → (Setof α)) α]
-           [(Pairof (α → Integer) (Integer → α))] . ->* . Void))]
+  [function-traces/tag (∀ (α τ) ([(α → (Setof α)) α] [(Bij α τ)] . ->* . Void))]
   [hash-traces (∀ (α) (HashTable α (Setof α)) α → Void)]
-  [hash-traces/tag
-   (∀ (α) ([(HashTable α (Setof α)) α]
-           [(Pairof (α → Integer) (Integer → α))] . ->* . Void))]
+  [hash-traces/tag (∀ (α τ) ([(HashTable α (Setof α)) α] [(Bij α τ)] . ->* . Void))]
   [make-int-tagger (∀ (α) → (Values (α → Integer) (Integer → α)))])
